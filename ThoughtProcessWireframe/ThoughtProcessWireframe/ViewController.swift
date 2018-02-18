e04f769beea356c07bf5d09dc22ea49f525cce17
gdudzic <gdudzic96@gmail.com>
2018-02-18 13:21:55 -0600

    Wireframe init

---
+++ ThoughtProcessWireframe/ThoughtProcessWireframe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThoughtProcessWireframe
+//
+//  Created by Gabriela Dudzic on 2/18/18.
+//  Copyright © 2018 Group3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
